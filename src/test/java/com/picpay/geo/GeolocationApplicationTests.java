b8281b834ca5ea72dc99d1ffe7f245042fd5ac6d
raphaellmario <raphaellmario@gmail.com>
2022-06-28 11:20:45 -0300

    init

---
+++ src/test/java/com/picpay/geo/GeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.geo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
